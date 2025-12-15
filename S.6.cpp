09cecceea155cb5019ed048ccf205061a5d4bb72
ThanawatThaotaku <rayar6513@gmail.com>
2025-12-15 10:00:27 +0700

    Add files via upload

---
+++ S.6.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int score;
+	scanf("%d",&score);
+	if (score > 100)
+		printf("error score");
+	else if (score >= 87)
+		printf("A");
+	else if (score >= 79)
+		printf("B");
+	else if (score >= 71)
+		printf("C");
+	else if (score >= 63)
+		printf("D");
+	else if (score >= 12)
+	printf("F");
+	
+	return 0;
+}
